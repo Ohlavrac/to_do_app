a23a09449acfa937c97def3b8001d0735dd22cea
ohlavrac <joaovicrcarvalho@gmail.com>
2024-09-09 10:21:01 -0300

    feat: add provider on app

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:to_do_app/data/datasources/local_datasource.dart';
+import 'package:to_do_app/data/repositories/task_repository.dart';
+import 'package:to_do_app/domain/usecases/save_task_usecase.dart';
+import 'package:to_do_app/presentation/providers/task_provider.dart';
 import 'package:to_do_app/presentation/screens/create_task_screen.dart';
 
 import 'presentation/screens/home_screen.dart';
@@ -8,12 +13,21 @@ class AppWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      initialRoute: "/home",
-      routes: {
-        "/home": (context) => const HomeScreen(),
-        "/createtask": (context) => const CreateTaskScreen()
-      }
+    final localDatasource = LocalDatasource();
+    final taskRepository = TaskRepository(local: localDatasource);
+    final saveTaskUsecase = SaveTaskUsecase(repository: taskRepository);
+    
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => TaskProvider(saveTaskUsecase: saveTaskUsecase))
+      ],
+      child: MaterialApp(
+        initialRoute: "/home",
+        routes: {
+          "/home": (context) => const HomeScreen(),
+          "/createtask": (context) => const CreateTaskScreen()
+        }
+      ),
     );
   }
 }
